9d75874ee37fb4b59f6215cf80b4bd458c266de9
Anderson Mendoza <davidmramos53@gmail.com>
2022-11-30 19:32:34 -0500

    Initial commit

---
+++ src/main/java/com/example/practica_en_clase/PracticaEnClaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica_en_clase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaEnClaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaEnClaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practica_en_clase/PracticaEnClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica_en_clase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaEnClaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
